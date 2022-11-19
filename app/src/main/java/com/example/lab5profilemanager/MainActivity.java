6eb412e538caafdba0b59b5164929549e71d69e7
Ousmane Barry <ppobarry@gmail.com>
2022-11-19 14:24:10 -0500

    add ability to pick flag (complete)

--- app/src/main/java/com/example/lab5profilemanager/MainActivity.java
+++ app/src/main/java/com/example/lab5profilemanager/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.lab5profilemanager;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
@@ -25,16 +32,53 @@ public class MainActivity extends AppCompatActivity {
         teamAddress = findViewById(R.id.teamAddress);
         openMaps = findViewById(R.id.openMaps);
 
+        ActivityResultLauncher<Intent> profileActivityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), this::onActivityResult);
 
         flag.setOnClickListener(view -> {
-            // open new ProfileActivity, choose flag, and display it here
-            Intent intent = new Intent(this, ProfileActivity.class);
-            startActivity(intent);
+            Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+            profileActivityResultLauncher.launch(intent);
         });
 
         openMaps.setOnClickListener(view -> {
             // open google maps using and display address provided in teamAddress
         });
 
+    }
+
+    public String getFlagName(int id) {
+        String drawableName;
+
+        if (id == R.id.france) {
+            drawableName = "flag_fr";
+        } else if (id == R.id.egypt) {
+            drawableName = "flag_eg";
+        } else if (id == R.id.spain) {
+            drawableName = "flag_sp";
+        } else if (id == R.id.korea) {
+            drawableName = "flag_kr";
+        } else if (id == R.id.japan) {
+            drawableName = "flag_jp";
+        } else if (id == R.id.turkey) {
+            drawableName = "flag_tr";
+        } else if (id == R.id.greatBritain) {
+            drawableName = "flag_uk";
+        } else if (id == R.id.unitedStates) {
+            drawableName = "flag_us";
+        } else {
+            drawableName = "flag_ca";
+        }
+
+        return drawableName;
+    }
+
+    public void onActivityResult(ActivityResult result) {
+        if (result.getResultCode() == Activity.RESULT_OK) {
+            Intent data = result.getData();
+            assert data != null;
+            int id = data.getIntExtra("imageID", R.id.canada);
+            String drawableName = getFlagName(id);
+            int resID = getResources().getIdentifier(drawableName, "drawable", getPackageName());
+            flag.setImageResource(resID);
+        }
     }
 }
--- app/src/main/java/com/example/lab5profilemanager/ProfileActivity.java
+++ app/src/main/java/com/example/lab5profilemanager/ProfileActivity.java
@@ -2,13 +2,40 @@ package com.example.lab5profilemanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        findViewById(R.id.france).setOnClickListener(this::setTeamIcon);
+        findViewById(R.id.egypt).setOnClickListener(this::setTeamIcon);
+        findViewById(R.id.canada).setOnClickListener(this::setTeamIcon);
+        findViewById(R.id.spain).setOnClickListener(this::setTeamIcon);
+        findViewById(R.id.korea).setOnClickListener(this::setTeamIcon);
+        findViewById(R.id.japan).setOnClickListener(this::setTeamIcon);
+        findViewById(R.id.turkey).setOnClickListener(this::setTeamIcon);
+        findViewById(R.id.greatBritain).setOnClickListener(this::setTeamIcon);
+        findViewById(R.id.unitedStates).setOnClickListener(this::setTeamIcon);
+
     }
+
+    public void setTeamIcon(View view) {
+        Intent returnIntent = new Intent();
+
+        ImageView selectedImage = (ImageView) view;
+
+        returnIntent.putExtra("imageID", selectedImage.getId());
+        setResult(RESULT_OK, returnIntent);
+
+        finish();
+    }
+
+
 }
